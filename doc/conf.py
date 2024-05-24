62b8b8d8d0ebe94ce62a25823cf716773a216e39
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-24 17:07:00 +0200

    hide name in sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -102,6 +102,7 @@ html_theme_options = {
             "class": "",
         },
     ],
+    "sidebar_hide_name": True,
 }
 
 # -- autosummary -----------------------------------------------------------------------
