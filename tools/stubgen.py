dff6a1a8235a5dda16fc1f64b0d6c26b3c83ba87
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-24 12:09:18 +0200

    fix workflows

--- tools/stubgen.py
+++ tools/stubgen.py
@@ -7,9 +7,9 @@ from pathlib import Path
 import isort
 from mypy import stubgen
 
-import template
+import nmod_wiki
 
-directory = Path(template.__file__).parent
+directory = Path(nmod_wiki.__file__).parent
 # remove existing stub files
 for file in directory.rglob("*.pyi"):
     file.unlink()
